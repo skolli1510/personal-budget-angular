628aea7cdb64cabd81bda88b8ca07b9cf769e791
skolli1510 <skolli8@charlotte.edu>
2024-09-21 08:29:58 +0530

    Added Changes related to instructions

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { ContactComponent } from './contact/contact.component';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
